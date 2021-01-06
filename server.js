aa5b3d83ae585dab754c585c7c206f50ee54b818
vinodell <max.khamitov@mail.ru>
2021-01-07 00:08:09 +0300

    env files added + static folder for static data

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+require('dotenv').config() // библиотека, позволяющая читать файл .env
+
 const server = express()
+const port = process.env.PORT || 8080 // берем переменную из .env
 
-const port = 8080
-
+server.use(express.static('public'))
+// отсюда выгружаем статические данные, которые не меняются
+// в зависимости от пользователя. Текст/картинки/стили
+server.use(express.json({ limit: '5mb' }))
 server.use((req, res, next) => {
   console.log(`${new Date()}: ${req.url} ${req.method} from ${req.ip}`)
   next()
 })
-
-server.use(bodyParser.json({ limit: '5mb'}))
 
 server.get('/', (req, res) => {
   res.send('express serv dude')
@@ -19,7 +21,7 @@ server.get('/name/:img', (req, res) => {
   res.send(`Hey, ${req.params.img}`)
 })
 
-server.post('/users', (req,res) => {
+server.post('/users', (req, res) => {
   const user = {
     name: req.body.name,
     age: req.body.age,
@@ -29,5 +31,5 @@ server.post('/users', (req,res) => {
 })
 
 server.listen(port, () => {
-  console.log(`Serving at http://localhost:${port}/`);
+  console.log(`Serving at http://localhost:${port}/`)
 })
