789e85c32711e58d285343c7f071c0331b4d2679
vinodell <max.khamitov@mail.ru>
2022-06-21 13:42:54 +0300

    need some updates with react-router-dom v6

--- client/config/root.js
+++ client/config/root.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Provider, useSelector } from 'react-redux'
+import { Provider } from 'react-redux'
 import {
-  Routes, Route, Navigate, BrowserRouter
+  Routes, Route, BrowserRouter
 } from 'react-router-dom'
 
 import store from '../redux'
@@ -9,21 +9,19 @@ import Startup from './startup'
 import Dummy from '../components/dummy'
 import Main from '../components/main'
 
-const OnlyAnonymousRoute = () => {
-  const { user, token } = useSelector((s) => s.auth)
-  const func = () => {
-    return !!user && !!token ? <Navigate to="/channels" /> : <Main />
-  }
-  return <Route element={func} />
+const TestCase = () => {
+  return <Route path="/go" element={<Main />} />
 }
 
-const PrivateRoute = () => {
-  const { user, token } = useSelector((s) => s.auth)
-  const func = () => {
-    return !!user && !!token ? <Main /> : <Navigate to="/login" />
-  }
-  return <Route element={func} />
-}
+// const OnlyAnonymousRoute = () => {
+//   const { user, token } = useSelector((s) => s.auth)
+//   return !!user && !!token ? <Main /> : <Main />
+// }
+
+// const PrivateRoute = () => {
+//   const { user, token } = useSelector((s) => s.auth)
+//   return !!user && !!token ? <Navigate to="/login" /> : <Route path="/lol" element={<Main />} />
+// }
 
 const Root = () => {
   return (
@@ -32,8 +30,7 @@ const Root = () => {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Dummy />} />
-            <Route path="anonymous" element={() => <OnlyAnonymousRoute />} />
-            <Route path="private" element={() => <PrivateRoute />} />
+            {TestCase()}
           </Routes>
         </BrowserRouter>
       </Startup>
